2776a2f066860f1503f7fbd5e43e1618f059494d
Matthew R <mattras82@gmail.com>
2020-12-07 21:49:22 -0700

    Day 5 solutions

---
+++ 05/two.js
@@ -0,0 +1,32 @@
+//@ts-check
+
+const getReduction = (str, min, max, i = 0) => {
+    const char = str[i];
+    const half = (max - min + 1) / 2;
+    const higher = ['B','R'].includes(char);
+    if (half > 1) {
+        min = higher ? (min + half) : min;
+        max = higher ? max : (min + half - 1);
+        return getReduction(str, min, max, i+1);
+    }
+    return higher ? max : min;
+};
+
+fetch('https://adventofcode.com/2020/day/5/input').then(r => {
+    r.text().then(t => {
+        const passes = t.split('\n').map(s => s.trim());
+        let answer = passes.map(p => {
+            let row = getReduction(p, 0, 127);
+            let col = getReduction(p, 0, 7, 7);
+            let id = row * 8 + col;
+            return id;
+        }).sort((a,b) => {
+            if (a > b) return 1;
+            if (b > a) return -1;
+            return 0;
+        }).find((id, i, arr) => {
+            return arr[i + 1] === id + 2;
+        });
+        console.log(`Your seat number is ${answer + 1}`);
+    });
+});
